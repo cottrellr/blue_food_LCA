19bd05884c3f867834d957b995e03b08f5f90e2a
liulikshun <htmiggabriel@gmail.com>
2022-05-25 23:44:59 +1000

    Added emissions per species based on production type
    Added emissions produced as a result of transportation

--- Production and Transporation Emissions.R
+++ Production and Transporation Emissions.R
@@ -5,8 +5,6 @@ library(tidyverse)
 
 #GHG Produced by Farmed Prawns
 
-write_csv(lca_model_dat, file = here("Outputs/lca_model_data_no-summary.csv"))
-
 lca_data <- read_csv(here("Outputs/lca_model_data_no-summary.csv")) %>% filter(taxa == "shrimp") %>% select(study_id, iso3c, clean_sci_name, taxa, intensity, system, total_ghg ) %>% arrange(iso3c)
 
 (lca_by_country <- lca_data %>% 
@@ -21,9 +19,6 @@ lca_data <- read_csv(here("Outputs/lca_model_data_no-summary.csv")) %>% filter(t
     select(iso3c, mean_ghg, sd_ghg))
 
 #GHG Produced by Wild-caught Prawns
-
-write_csv(wild_dat_new_weights, file = here("Outputs/wild_data_GHG_Model.csv"))
-
 
 wild_ghg_data <- read_csv(here("Outputs/wild_data_GHG_Model.csv")) %>% filter(taxa == "Shrimps") %>% select(species, gear, ghg, clean_sci_name, taxa)
 
@@ -50,13 +45,13 @@ Country_Mean_1 <- lca_data %>% group_by(iso3c) %>% summarise(mean_ghg = mean(tot
 
 #Overall mean for other countries (Farmed Prawns)
 
-target <- c("CHN","IDN","THA","VNM")
+target <- c("BGD","IDN","THA","VNM")
 
-#WHAT IS THE REASON FOR USING BANGLADESH AS AVERAGE?
+#We use China as an average as they represent a range of different prawn farming systems (Intensive & Semi-intensive)
 
 Country_mean_2 <- read_csv(here("Outputs/lca_model_data_no-summary.csv"))%>% filter(taxa == "shrimp") %>% filter(!iso3c %in% target) %>% 
   select(study_id, iso3c, clean_sci_name, taxa, intensity, system, total_ghg ) %>% arrange(iso3c) %>% group_by(iso3c) %>% summarise(mean_ghg = mean(total_ghg))%>%
-  mutate(iso3c = replace(iso3c, iso3c == "BGD", "Other_Countries_Farmed"))
+  mutate(iso3c = replace(iso3c, iso3c == "CHN", "Other_Countries_Farmed"))
 
 Country_Mean_Farmed <- rbind(Country_Mean_1,Country_mean_2)
 
@@ -68,17 +63,11 @@ GHG_EF <- bind_rows(total_ghg_wild,Mean_GHG_farmed)
 
 #Transportation
 
+#Add in emissions factors from the Fourth IMO GHG Study, we used values from 2013 for container ships.
 
+#Link: https://wwwcdn.imo.org/localresources/en/OurWork/Environment/Documents/Fourth%20IMO%20GHG%20Study%202020%20-%20Full%20report%20and%20annexes.pdf
 
-#Lower quartile: 15.2, Upper Quartile: 20.1 as per IMO GHG Report
-
-
-#The emissions should go in its own csv and be read in
-Emissions_Range <- c(35.2, 27.3,
-                     20.3, 17.7, 16.6, 13.4, 10.6, 7.3)
-
-
-
+#Add in redundant trade values per country with Australia 
 
 transport_distance_volume_spp <- read_csv("data/Transportation_Distance_Per_Species.csv")
 
@@ -91,6 +80,8 @@ transport_distance_volume_spp_list <- transport_distance_volume_spp %>%
 transport_output_list <- list()
 total_output_list <- list()
 
+calc_df %>% select(iso3c) %>% unique()
+
 set.seed(333)
 
 for(i in 1:500){
@@ -99,70 +90,57 @@ for(i in 1:500){
   
   (calc_df <- transport_distance_volume_spp %>% 
     mutate(ef = ef) %>% 
-    mutate(next_column = quantity*distance*ef*2) %>% 
-    mutate(final_column = next_column/0.5*1000) %>% 
+    mutate(emissions_nautical_mile = quantity*distance*ef*2) %>% 
+    mutate(total_emissions = emissions_nautical_mile*0.54/1000) %>% 
     mutate(rep = i))
   
   output_df_list[[i]] <- calc_df
 
-  #add in production
+#add in production data
   
-  total_calc_df <- calc_df %>% 
-    envioronmental_data. #whatever this is
+Country_Mean_Farmed <- Country_Mean_Farmed %>% mutate(prod_method = "Farmed")
   
-  total_output_list[[i]] <- total_calc_df
+redundant_trade_emissions <- merge(Country_Mean_Farmed,calc_df, by = c("iso3c","prod_method"), all.y = T) %>% view()
   
-}
+#Add in production GHG for wild caught
+
+total_ghg_wild <- total_ghg_wild[,c(1,3,2)]
+
+#remove the iso3c
+
+total_ghg_wild <- select(total_ghg_wild, -c(iso3c))
+
+redundant_trade_emissions <- left_join(redundant_trade_emissions,total_ghg_wild, by = c("prod_method")) %>% view()
+
+redundant_trade_emissions <- redundant_trade_emissions %>% unite(mean_ghg,c("mean_ghg.y", "mean_ghg.x"), na.rm = TRUE) %>% view()
+
+#fill blanks 
+
+redundant_trade_emissions <-redundant_trade_emissions %>% mutate(mean_ghg = ifelse(mean_ghg %in% "", 5647.98129728448
+,mean_ghg)) %>% view()
+
   
-emissions_outputfile <- bind_rows(output_df_list)  
-
-write_csv(x = emissions_outputfile, file = here("data/output/emissions_output_w_reps.csv"))
-
-
-emissions_outputfile %>% 
-  group_by(iso3c, species_name, prod_method) %>% 
-  summarise(estimate = mean(final_column, na.rm= TRUE),
-            sd = sd(final_column, na.rm = TRUE))
-  
-  
-
-
-#SD is 1.457738
-
-mean(Emissions_Range)
-
-#mean is 17.65
-
-#COnvert to Kg CO2e tonne-1 km-1 
-
-Emissions_Range <- Emissions_Range*0.54/1000
-
-mean(Emissions_Range)
-
-#0.009531
-
-sd(Emissions_Range)
-
-#0.0007871
-
-emissions_factor <- rnorm(n=2000, mean = 0.009531, sd=0.0007871)
-
-mean(emissions_factor)
-
-
-for(i in 1:1000){
-  
-total_km_travelled <- (763977*2)
-  
-total_trade_volume <- 58296.2354
-  
-calc <- emissions_factor*total_trade_volume*total_km_travelled
-
-output <- c(output, calc)
+  total_output_list[[i]] <- redundant_trade_emissions
   
 }
+  
+write_csv(x = redundant_trade_emissions, file = here("data/output/emissions_output_w_reps.csv"))
 
-simple_ouput <- unlist(output)
+#Emission per species
+emissions_per_species <-redundant_trade_emissions %>% 
+  group_by(species_name, prod_method) %>% 
+  summarise(species_emission = sum(total_emissions, na.rm= TRUE)) %>% view()
 
-mean(simple_ouput)
-sd(simple_ouput)
+sum(emissions_per_species$species_emission)
+
+#Total emissions produced from transportation is 4730576 kg CO2
+
+redundant_trade_emissions %>% 
+  group_by(species_name, prod_method) %>% 
+  summarise(estimate = mean(total_emissions, na.rm= TRUE),
+            sd = sd(total_emissions, na.rm = TRUE)) %>% view()
+
+#Plots
+
+#In the interest of time, I might just plot using excel first instead of R
+  
